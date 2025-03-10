1a01d4fdaab8c3658bcaeed56f3ba1d0435a6ad9
mpiirs <mpiirs@taltech.ee>
2025-03-10 18:23:06 +0200

    modified Seat Entity, many-to-many relationship with adjacent seats, many-to-one relationship with flight plan

--- src/main/java/cgi/backend/seats/Seat.java
+++ src/main/java/cgi/backend/seats/Seat.java
@@ -1,10 +1,12 @@
 package cgi.backend.seats;
 
+import cgi.backend.flightplan.FlightPlan;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,7 +22,9 @@ public class Seat {
   private Boolean nextToWindow;
   private Boolean moreLegSpace;
   private Boolean closeToExit;
-  @OneToMany
+  @ManyToMany
   private Set<Seat> adjacentSeats;
+  @ManyToOne
+  private FlightPlan flight;
 
 }
