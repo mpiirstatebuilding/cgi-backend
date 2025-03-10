e2ba3174d481317cc7fc33e8f6aafb17cd03fb6c
mpiirs <mpiirs@taltech.ee>
2025-03-10 12:06:14 +0200

    init

---
+++ src/main/java/cgi/backend/CgiBackendApplication.java
@@ -0,0 +1,13 @@
+package cgi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CgiBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cgi/backend/CgiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cgi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
